f4ad7c598fcf5669c80a3e3345843160a840ba1b
Olivia Auzenne <oauzenne@gmail.com>
2019-03-09 14:48:29 -0600

    Add files via upload

---
+++ bin/run.rb
@@ -0,0 +1,23 @@
+require_relative '../config/environment'
+puts "\e[H\e[2J"
+
+def greeting
+puts "Welcome to ZOLI POST! A place to create and motivate."
+puts ""
+login
+end
+
+greeting
+
+
+
+
+
+
+
+
+
+
+
+
+
