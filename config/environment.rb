0249aca52020efb589caf1f99734700a2b9855ce
Olivia Auzenne <oauzenne@gmail.com>
2019-03-09 14:48:43 -0600

    Add files via upload

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require 'bundler'
+Bundler.require
+
+ActiveRecord::Base.logger = nil
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+
+require_all 'lib'
+require_all 'app'
+
+
